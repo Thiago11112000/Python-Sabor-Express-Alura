71932b3f18ea612b88f983af971ef5a34810cdc9
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-08-23 17:51:24 -0300

    Estilizando a visualização

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
+    print('3. alternar estado do restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
@@ -34,7 +34,10 @@ def opcao_invalida():
 
 def exibir_subtitulo(texto):
     os.system('cls' if os.name == 'nt' else 'clear')
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print (linha)
     print()
 
 def cadastrar_novo_restaurante():
@@ -48,11 +51,12 @@ def cadastrar_novo_restaurante():
 
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
+    print(f"{'Nome do Restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | Status")
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
-        ativo = 'ativo' if restaurante['ativo'] else 'inativo'
-        print(f'• {nome_restaurante} | {categoria} | {ativo}')
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'{nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
     voltar_ao_menu_principal()
 
 def alternar_estado_restaurante():
